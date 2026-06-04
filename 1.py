c9df6d3069f5e9101b2e72fdf19c7c97093b5c89
RussianFrobeR <96429021+RussianFrobeR@users.noreply.github.com>
2026-06-04 14:24:55 +0800

    1.py

--- 1.py
+++ 1.py
@@ -11,12 +11,12 @@ def solve_quadratic_equation(a, b, c):
         x1 = (-b + math.sqrt(D)) / (2.0 * a)
         x2 = (-b - math.sqrt(D)) / (2.0 * a)
 
-        print(x1 + x2)
+        print(x1, x2)
 
         print("Проверка по теореме Виета:")
 
-        print((x1 + x2) + (-b / a))
-        print((x1 * x2) + (c / a))
+        print((x1 + x2), (-b / a))
+        print((x1 * x2), (c / a))
 
         if (abs((x1 + x2) - (-b / a)) < 1e-10 and
             abs((x1 * x2) - (c / a)) < 1e-10):
@@ -24,22 +24,8 @@ def solve_quadratic_equation(a, b, c):
         else:
             print("Все плохо :(")
 
-
-def get_quadratic_roots(a, b, c):
-    D = b * b - 4 * a * c
-    if D < 0:
-        return []
-    elif D == 0:
-        x = -b / (2.0 * a)
-        return [x]
-    else:
-        x1 = (-b + math.sqrt(D)) / (2.0 * a)
-        x2 = (-b - math.sqrt(D)) / (2.0 * a)
-        return [x1, x2]
-
-
 if __name__ == "__main__":
     a = 1
-    b = 2
-    c = 1
+    b = 6
+    c = 2
     solve_quadratic_equation(a, b, c)
